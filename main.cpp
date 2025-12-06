fcce23c208434023ce8ed2aeb72d1124f062bd33
kretorek-droid <sebastianwergj@gmail.com>
2025-12-06 09:58:15 +0100

    nextupdate

--- main.cpp
+++ main.cpp
@@ -22,45 +22,15 @@ int main() {
     // Z NICH. WYŚWIETL SUMĘ PZOOSTAŁYCH LICZB TYLE RAZY ILE WYNOSI WARTOŚĆ NAJWIĘKSZEJ LICZBY
     int a, b, c;
 
-    cout << "Podaj 3 liczby calkowite nieujemne" << endl;
-    cin >> a >> b >> c;
 
-    // if (a < 0 || b < 0 || c < 0) {
-    //     cout << "wszystkie liczby nie są nieujemne, koniec programu" << endl;
-    //     return 0;
-    // }
-
-
-
-    if (a >=0 && b >=0 && c >=0) {
-        cout << "a:" << a <<  endl;
-        cout << "b:" << b << endl;
-        cout << "c:" << c << endl;
-
-        int nasz_max = wartoscMax(a, b, c);
-        int suma_pozostalych = a + b + c - nasz_max;
-        cout << "nasz_max :" << nasz_max  << endl;
-        cout << "suma_pozostalych :" << suma_pozostalych << endl;
-
-        for (int i = 0; i < nasz_max; i++) {
-            cout << suma_pozostalych << endl;
-        }
-
-
-
-    } else {
-        cout << "wszystkie liczby nie sa nieujemne, koniec programu" << endl;
-    }
+   //wywolanieZadania11();
+   //wywolanieZadania16();
+   //wywolanieZadania119();
+    wywolanieZadania17();
 
 
 
 
 
 
-
-
-
-
-
-    return 0;
 }
